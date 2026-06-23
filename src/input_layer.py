aae002527f8bb288f3d820ddb0ba9130ee5c296b
Muhammad Usman <muhammadusman5805@gmail.com>
2026-06-23 15:51:34 +0100

    Task 3

--- src/input_layer.py
+++ src/input_layer.py
@@ -5,3 +5,13 @@ def parse_plateau(text):
     max_y = int(parts[1])
 
     return {"max_x": max_x, "max_y": max_y}
+
+
+def parse_position(text):
+    parts = text.split()
+
+    x = int(parts[0])
+    y = int(parts[1])
+    direction = parts[2]
+
+    return {"x": x, "y": y, "direction": direction}
--- tests/test_input_layer.py
+++ tests/test_input_layer.py
@@ -1,4 +1,4 @@
-from src.input_layer import parse_plateau
+from src.input_layer import parse_plateau, parse_position
 
 
 def test_parse_plateau_returns_dictionary():
@@ -11,3 +11,39 @@ def test_parse_plateau_works_with_different_size():
     result = parse_plateau("8 10")
 
     assert result == {"max_x": 8, "max_y": 10}
+
+
+def test_parse_position_returns_dictionary():
+    result = parse_position("1 2 N")
+
+    assert result == {"x": 1, "y": 2, "direction": "N"}
+
+
+def test_parse_position_works_with_different_position():
+    result = parse_position("3 4 E")
+
+    assert result == {"x": 3, "y": 4, "direction": "E"}
+
+
+def test_parse_position_can_parse_north():
+    result = parse_position("0 0 N")
+
+    assert result == {"x": 0, "y": 0, "direction": "N"}
+
+
+def test_parse_position_can_parse_east():
+    result = parse_position("0 0 E")
+
+    assert result == {"x": 0, "y": 0, "direction": "E"}
+
+
+def test_parse_position_can_parse_south():
+    result = parse_position("0 0 S")
+
+    assert result == {"x": 0, "y": 0, "direction": "S"}
+
+
+def test_parse_position_can_parse_west():
+    result = parse_position("0 0 W")
+
+    assert result == {"x": 0, "y": 0, "direction": "W"}
